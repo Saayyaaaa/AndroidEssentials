59f25f912a533306de222a4d71fcb9d92b1073d1
SAYA <sajkasajko@gmail.com>
2025-02-27 17:44:53 +0500

    получаем рисунок из галереи

--- app/src/main/java/com/example/androidessentials/IntentsDeepLinking.kt
+++ app/src/main/java/com/example/androidessentials/IntentsDeepLinking.kt
@@ -1,59 +1,50 @@
 package com.example.androidessentials
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [IntentsDeepLinking.newInstance] factory method to
- * create an instance of this fragment.
- */
 class IntentsDeepLinking : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    val REQUEST_CODE = 1
+    private lateinit var imageView: ImageView
+    private lateinit var galleryBtn: Button
+
+    private var selectedImageUri: Uri? = null
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_intents_deep_linking, container, false)
+        val view = inflater.inflate(R.layout.fragment_intents_deep_linking, container, false)
+        imageView = view.findViewById(R.id.imageView)
+        galleryBtn = view.findViewById(R.id.galleryBtn)
+
+
+        galleryBtn.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(intent, REQUEST_CODE)
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment IntentsDeepLinking.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            IntentsDeepLinking().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null) {
+            selectedImageUri = data.data  // Получаем URI выбранного изображения
+            imageView.setImageURI(selectedImageUri)  // Устанавливаем изображение в ImageView
+        }
     }
 }
